2b541270630e105350bd3bb43aff0f988fa8ba20
Handuo <handuo@server31.io>
2026-07-10 00:08:59 +0800

    Enable text copy in Mac app

--- scripts/install-mac-app.js
+++ scripts/install-mac-app.js
@@ -67,6 +67,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, WKNavigationDelegate {
 
   func applicationDidFinishLaunching(_ notification: Notification) {
     NSApp.setActivationPolicy(.regular)
+    createMenu()
     createWindow()
     log("app launched")
     startHostIfNeeded()
@@ -81,6 +82,30 @@ final class AppDelegate: NSObject, NSApplicationDelegate, WKNavigationDelegate {
     if let process = hostProcess, process.isRunning {
       process.terminate()
     }
+  }
+
+  private func createMenu() {
+    let mainMenu = NSMenu()
+
+    let appMenuItem = NSMenuItem()
+    let appMenu = NSMenu()
+    appMenu.addItem(NSMenuItem(title: "Quit Cinder", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+    appMenuItem.submenu = appMenu
+    mainMenu.addItem(appMenuItem)
+
+    let editMenuItem = NSMenuItem()
+    let editMenu = NSMenu(title: "Edit")
+    editMenu.addItem(NSMenuItem(title: "Undo", action: Selector(("undo:")), keyEquivalent: "z"))
+    editMenu.addItem(NSMenuItem(title: "Redo", action: Selector(("redo:")), keyEquivalent: "Z"))
+    editMenu.addItem(NSMenuItem.separator())
+    editMenu.addItem(NSMenuItem(title: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x"))
+    editMenu.addItem(NSMenuItem(title: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c"))
+    editMenu.addItem(NSMenuItem(title: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v"))
+    editMenu.addItem(NSMenuItem(title: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a"))
+    editMenuItem.submenu = editMenu
+    mainMenu.addItem(editMenuItem)
+
+    NSApp.mainMenu = mainMenu
   }
 
   private func createWindow() {
